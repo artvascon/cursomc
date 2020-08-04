4e43b562fee22c7b34992917a99073fc12ef29e8
Arthur Vasconcelos <artvascon@hotmail.com>
2020-08-04 14:39:42 -0300

    Revert "Ajustes no endpoint /categorias/{id}"
    This reverts commit c98093fff294f62d085a9950c0b68264c5dbe88d.

--- src/main/java/com/vascompany/cursomc/repositories/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vascompany.cursomc.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vascompany.cursomc.domain.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
-}
